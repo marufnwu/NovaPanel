700e9963c984cd6439f0a70283c2f7f7704e513b
Maruf Ahmed <dev00.maruf@gmail.com>
2026-05-25 01:52:49 +0600

    fix: remove ToastProvider from AppLayout to isolate useSyncExternalStore error

--- apps/web/src/components/layout/AppLayout.tsx
+++ apps/web/src/components/layout/AppLayout.tsx
@@ -2,12 +2,10 @@ import { Outlet } from '@tanstack/react-router';
 import { Sidebar } from './Sidebar';
 import { Topbar } from './Topbar';
 import { StatusBar } from './StatusBar';
-import { ToastProvider } from '../ui/ToastProvider';
 
 export function AppLayout() {
   return (
     <>
-      <ToastProvider />
       <div className="h-screen flex overflow-hidden">
         <Sidebar />
         <div className="flex-1 flex flex-col min-w-0">
